1bbcd3db8ab698916cbd42c0f832d6f7c6bab3e7
Wasif Qayyum <air@wasif-qayyum.local>
2016-11-03 22:44:58 +0500

    Initial Commit

---
+++ Template/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Template
+//
+//  Created by Wasif Qayyum on 11/3/16.
+//  Copyright © 2016 Wasif Qayyum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Template/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Template
+//
+//  Created by Wasif Qayyum on 11/3/16.
+//  Copyright © 2016 Wasif Qayyum. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
